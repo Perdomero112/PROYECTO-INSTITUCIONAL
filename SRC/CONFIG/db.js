bc58d99480c1cc5c3c7a9c3f7be72aeff0399114
perdomero <samups2007@gmail.com>
2025-09-20 12:03:27 -0500

    configuracion para desplegar temporalmente .env

--- SRC/CONFIG/db.js
+++ SRC/CONFIG/db.js
@@ -1,11 +1,14 @@
 const mysql = require("mysql2");
+const dotenv = require("dotenv");
+
+dotenv.config();
 
 const conexion = mysql.createConnection({
-    host: process.env.DB_HOST || "localhost",
-    database: process.env.DB_NAME || "biblioteca_ie-bg",
-    user: process.env.DB_USER || "root",
-    password: process.env.DB_PASSWORD || "",
-    port: process.env.DB_PORT ? Number(process.env.DB_PORT) : 3306
+    host: process.env.DB_HOST, //host de la base de datos VPS 
+    database: process.env.DB_NAME, //nombre de la base de datos
+    user: process.env.DB_USER, //usuario de la base de datos
+    password: process.env.DB_PASSWORD, //contraseña de la base de datos
+    port: process.env.DB_PORT ? Number(process.env.DB_PORT) : 3306 //puerto de la base de datos
 });
 
 conexion.connect((err) => {
