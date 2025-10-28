fe495e62e728c8fc465e0d0419d15d8a5eb5fa74
perdomero <samups2007@gmail.com>
2025-10-28 00:25:54 -0500

    update_db_config

--- SRC/CONFIG/db.js
+++ SRC/CONFIG/db.js
@@ -7,7 +7,7 @@ dotenv.config();
 // Usar process.env para permitir configuración en la VPS / producción.
 const conexion = mysql.createConnection({
     host: process.env.DB_HOST || 'localhost',
-    database: process.env.DB_DATABASE || 'biblioteca_ie-bg',
+    database: process.env.DB_DATABASE || process.env.DB_NAME || 'biblioteca_ie_bg',
     user: process.env.DB_USER || 'root',
     password: process.env.DB_PASSWORD || '',
     port: process.env.DB_PORT ? Number(process.env.DB_PORT) : 3306,
